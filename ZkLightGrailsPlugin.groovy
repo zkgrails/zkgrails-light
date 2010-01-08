c71cb83dccecd015986afb8a0939161cbdd7efd2
Chanwit Kaewkasi <chanwit@gmail.com>
2010-01-08 21:07:42 +0000

    updated plugin

--- ZkLightGrailsPlugin.groovy
+++ ZkLightGrailsPlugin.groovy
@@ -7,7 +7,12 @@ class ZkLightGrailsPlugin {
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
+    	"grails-app/controllers/**/Test*.groovy",
+    	"grails-app/domain/**/Test*.groovy",
         "grails-app/views/error.gsp",
+        "test/**",
+        "web-app/WEB-INF/*",
+        "web-app/test/*",
         "copy.sh"
     ]
 
